fa3bcc47aed5f4e88162402eb37d87ee2f79e287
Sheldon <Sheld.chan@gmail.com>
2019-06-22 16:05:15 -0700

    Create Artworks table.

--- routes/db/schema.rb
+++ routes/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_22_224533) do
+ActiveRecord::Schema.define(version: 2019_06_22_225447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image_url", null: false
+    t.integer "artist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title", "artist_id"], name: "index_artworks_on_title_and_artist_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
